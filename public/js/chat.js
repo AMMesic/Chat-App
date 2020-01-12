32afe944398728b3526ca9bd02727d79fc593b57
AMMesic <54796060+AMMesic@users.noreply.github.com>
2020-01-13 00:52:23 +0100

    Add message when new user joins

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,6 +9,6 @@ socket.on('message', (message) => {
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    const message = e.target.value
+    const message = e.target.elements.message.value
     socket.emit('sendMessage', message)
 })
