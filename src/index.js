88ca0dabcafff73e4f1db67fff996c35dcb7c168
AMMesic <54796060+AMMesic@users.noreply.github.com>
2020-01-14 21:46:04 +0100

    Add helper functions in server for users, so users in the same room can chat and users that aren't in the same room can't see that specific room

--- src/index.js
+++ src/index.js
@@ -26,38 +26,59 @@ app.use(express.static(publicDirectoryPath));
 io.on('connection', socket => {
   console.log('New WebSocket connection');
 
-  socket.on('join', ({ username, room }) => {
-    socket.join(room);
+  socket.on('join', (options, callback) => {
+    const { error, user } = addUser({ id: socket.id, ...options });
+
+    if (error) {
+      return callback(error);
+    }
+
+    socket.join(user.room);
 
     socket.emit('message', generateMessage('Welcome!'));
     socket.broadcast
-      .to(room)
-      .emit('message', generateMessage(`${username} has joined ${room}`));
+      .to(user.room)
+      .emit(
+        'message',
+        generateMessage(`${user.username} has joined ${user.room}`)
+      );
+
+    callback();
   });
 
   socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
     const filter = new Filter();
 
     if (filter.isProfane(message)) {
       return callback('Profanity is not allowed!');
     }
 
-    io.to('A').emit('message', generateMessage(message));
+    io.to(user.room).emit('message', generateMessage(message));
     callback();
   });
 
-  socket.on('disconnect', () => {
-    io.emit('message', generateMessage('A user has left the chat!'));
-  });
-
   socket.on('sendLocation', (location, callback) => {
-    io.emit(
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit(
       'locationMessage',
       generateLocationMessage(
         `https://google.com/maps?q=${location.latitude},${location.longitude}`
       )
     );
     callback();
+  });
+  
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        generateMessage(`${user.username} has left the chat!`)
+      );
+    }
   });
 });
 
